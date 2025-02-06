bafc55d279319c6e94108dc888a59d746d64fddb
hrishikesh8848 <hrishikesh@8848digital.com>
2025-02-06 13:00:50 +0530

    log: remove unnecessary logs

--- frappe/core/doctype/data_import/importer.py
+++ frappe/core/doctype/data_import/importer.py
@@ -72,7 +72,6 @@ class Importer:
 		self.data_import.db_set("template_warnings", "")
 
 	def import_data(self):
-		frappe.log_error("importt")
 		self.before_import()
 
 		# parse docs from rows
@@ -178,7 +177,6 @@ class Importer:
 					)
 
 					log_index += 1
-					frappe.log_error("import loggg")
 					if not self.data_import.status == "Partial Success":
 						self.data_import.db_set("status", "Partial Success")
 
@@ -189,7 +187,6 @@ class Importer:
 					messages = frappe.local.message_log
 					frappe.clear_messages()
 					# rollback if exception
-					frappe.log_error("import exception")
 					if self.doctype != "Bank Transaction":
 						frappe.db.rollback()
 
