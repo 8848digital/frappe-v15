fbe84afe157e9def5675b93a120cac47953c743a
shubham-DT <shubham@8848digital.com>
2025-02-14 10:56:04 +0530

    feat: allow created a session for a fixed duration via bench browse

--- frappe/commands/site.py
+++ frappe/commands/site.py
@@ -1150,7 +1150,7 @@ def publish_realtime(context, event, message, room, user, doctype, docname, afte
 @click.argument("site", required=False)
 @click.option("--user", required=False, help="Login as user")
 @pass_context
-def browse(context, site, user=None):
+def browse(context, site, user=None, duration=None):
 	"""Opens the site on web browser"""
 	from frappe.auth import CookieManager, LoginManager
 
@@ -1165,6 +1165,8 @@ def browse(context, site, user=None):
 
 	frappe.init(site=site)
 	frappe.connect()
+ 
+	frappe.flags.session_duration = duration
 
 	sid = ""
 	if user:
