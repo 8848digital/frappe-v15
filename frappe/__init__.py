2f1ab89b80f9bfa3e4abf44415655178e9dbd48b
Abhishek8848 <abhishek.p@8848digital.com>
2025-02-21 19:55:11 +0530

    Added: comment

--- frappe/__init__.py
+++ frappe/__init__.py
@@ -862,6 +862,7 @@ def whitelist(allow_guest=False, xss_safe=False, methods=None):
 		else:
 			fn = validate_argument_types(fn, apply_condition=in_request_or_test)
 
+		#convert set to list fixed
 		whitelisted.add(fn)
 		allowed_http_methods_for_whitelisted_func[fn] = methods
 
