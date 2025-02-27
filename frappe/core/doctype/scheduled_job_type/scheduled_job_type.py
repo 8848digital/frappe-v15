35b07f175f92be860cf6a146b6bcab41d602b786
hrishikesh8848 <hrishikesh@8848digital.com>
2025-02-27 17:47:36 +0530

    fix:scheduled job issue

--- frappe/core/doctype/scheduled_job_type/scheduled_job_type.py
+++ frappe/core/doctype/scheduled_job_type/scheduled_job_type.py
@@ -255,7 +255,7 @@ def insert_single_event(frequency: str, event: str, cron_format: str | None = No
 			frappe.db.savepoint("scheduled_job_type_creation")
 			doc.insert()
 		except frappe.DuplicateEntryError:
-			frappe.db.rollback(save_point="scheduled_job_type_creation")
+			frappe.db.rollback("scheduled_job_type_creation")
 			doc.delete()
 			doc.insert()
 
