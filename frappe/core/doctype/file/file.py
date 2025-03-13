6d9fe4b44752beebd7f1aacd947c9da84f0af187
Barathpalanisamy <barath@8848digital.com>
2025-03-13 11:16:48 +0530

    fix: check if duplicate_file url matches incoming file url

--- frappe/core/doctype/file/file.py
+++ frappe/core/doctype/file/file.py
@@ -380,8 +380,11 @@ class File(Document):
 			filters = {
 				"content_hash": self.content_hash,
 				"is_private": self.is_private,
-				"name": ("!=", self.name),
 			}
+
+			if self.name:
+				filters.update({"name": ("!=", self.name)})
+
 			if self.attached_to_doctype and self.attached_to_name:
 				filters.update(
 					{
@@ -650,7 +653,8 @@ class File(Document):
 		if duplicate_file:
 			file_doc: "File" = frappe.get_cached_doc("File", duplicate_file.name)
 			if file_doc.exists_on_disk():
-				self.file_url = duplicate_file.file_url
+				if self.make_file() == duplicate_file.file_url:
+					self.file_url = duplicate_file.file_url
 				file_exists = True
 
 		if not file_exists:
@@ -675,6 +679,15 @@ class File(Document):
 		fpath = self.write_file()
 
 		return {"file_name": os.path.basename(fpath), "file_url": self.file_url}
+
+	def make_file_url(self):
+		file_url = None
+		safe_file_name = re.sub(r"[/\\%?#]", "_", self.file_name)
+		if self.is_private:
+			file_url = f"/private/files/{safe_file_name}"
+		else:
+			file_url = f"/files/{safe_file_name}"
+		return file_url
 
 	def check_max_file_size(self):
 		from frappe.core.api.file import get_max_file_size
