33a32560e0833872d48d601a79e21110f2faa5a7
Barathpalanisamy <barath@8848digital.com>
2025-03-17 12:25:11 +0530

    fix: use total_row flag to check

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -112,7 +112,8 @@ def generate_report_result(
 		result = add_total_row(result, columns, is_tree=is_tree, parent_field=parent_field)
 
 	if isinstance(filters, dict) and filters.get("translate_data"):
-		result = translate_report_data(result)
+		total_row = cint(report.add_total_row) and result and not skip_total_row
+		result = translate_report_data(result, total_row)
 
 	return {
 		"result": result,
