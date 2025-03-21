9bea72fdeb019159583a88af7db865466de0d6d3
Barathpalanisamy <barath@8848digital.com>
2025-03-21 16:56:13 +0530

    fix(user): make attachments in email signature public

--- frappe/model/db_query.py
+++ frappe/model/db_query.py
@@ -15,7 +15,7 @@ import frappe.share
 from frappe import _
 from frappe.core.doctype.server_script.server_script_utils import get_server_script_map
 from frappe.database.utils import DefaultOrderBy, FallBackDateTimeStr, NestedSetHierarchy
-from frappe.model import OPTIONAL_FIELDS, get_permitted_fields
+from frappe.model import OPTIONAL_FIELDS, get_permitted_fields, optional_fields
 from frappe.model.meta import get_table_columns
 from frappe.model.utils import is_virtual_doctype
 from frappe.model.utils.user_settings import get_user_settings, update_user_settings
