66099ab0d1fe94df17362ad71faf3815cf0b6d33
Barathpalanisamy <barath@8848digital.com>
2025-03-24 17:55:32 +0530

    fix: add a condition for postgres

--- frappe/patches/v14_0/fix_user_settings_collation.py
+++ frappe/patches/v14_0/fix_user_settings_collation.py
@@ -1,5 +1,10 @@
 import frappe
 
-
 def execute():
-	frappe.db.sql("ALTER TABLE __UserSettings CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+    if frappe.db.db_type == "mariadb":
+        frappe.db.sql("ALTER TABLE __UserSettings CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
+    elif frappe.db.db_type == "postgres":
+        frappe.db.sql("""
+            ALTER TABLE "__UserSettings" 
+            ALTER COLUMN "value" TYPE TEXT COLLATE "en_US.utf8";
+        """)
