e36f989c90fa0536ff9bd53bf78dfd8548ca66d5
Barathpalanisamy <barath@8848digital.com>
2025-03-25 13:53:29 +0530

    fix: patch update

--- frappe/patches/v14_0/fix_user_settings_collation.py
+++ frappe/patches/v14_0/fix_user_settings_collation.py
@@ -4,7 +4,8 @@ def execute():
     if frappe.conf.db_type == "mariadb":
         frappe.db.sql("ALTER TABLE `__UserSettings` CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;")
     elif frappe.conf.db_type == "postgres":
-        frappe.db.sql("""
+        default_collation = frappe.db.sql("SHOW LC_COLLATE", as_dict=True)[0]["lc_collate"]
+        frappe.db.sql(f"""
             ALTER TABLE "__UserSettings" 
-            ALTER COLUMN "data" TYPE TEXT COLLATE "C.UTF-8";
+            ALTER COLUMN "data" TYPE TEXT COLLATE "{default_collation}";
         """)
