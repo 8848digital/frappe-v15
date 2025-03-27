3dea61d480512b7e55b223285075a8808b122e87
Nikhil Kothari <nik.kothari22@live.com>
2025-03-27 09:49:55 +0530

    fix(socketio): use auth header if available when making permission requests (#31926)
    * fix(socketio): pass auth token if available instead of cookie
    
    * chore: linting
    
    * fix(socketio): use auth header if available when making permission requests

--- realtime/utils.js
+++ realtime/utils.js
@@ -11,8 +11,6 @@ function get_url(socket, path) {
 function frappe_request(path, socket) {
 	const partial_req = request.get(get_url(socket, path));
 	if (socket.authorization_header) {
-
-
 		return partial_req.set("Authorization", socket.authorization_header);
 	} else if (socket.sid) {
 		return partial_req.query({ sid: socket.sid });
