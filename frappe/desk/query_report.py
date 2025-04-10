130aef1fac7b4bf7eb08d1bd1a92eedf7af43af7
Barathpalanisamy <barath@8848digital.com>
2025-04-10 10:41:13 +0530

    fix: restructure logic to generate report name

--- frappe/desk/query_report.py
+++ frappe/desk/query_report.py
@@ -360,15 +360,14 @@ def export_query():
 		content = make_xlsx(xlsx_data, "Query Report", column_widths=column_widths).getvalue()
 
 	for value in (data.filters or {}).values():
-		if isinstance(value, list) and valid_report_name(report_name, "_" + ",".join(value)):
-			report_name += "_" + ",".join(value)
+		suffix = ""
+		if isinstance(value, list):
+			suffix = "_" + ",".join(value)
+		elif isinstance(value, str) and value not in {"Yes", "No"}:
+			suffix = f"_{value}"
 
-		elif (
-			isinstance(value, str)
-			and value not in {"Yes", "No"}
-			and valid_report_name(report_name, f"_{value}")
-		):
-			report_name += f"_{value}"
+		if valid_report_name(report_name, suffix):
+			report_name += suffix
 
 	provide_binary_file(report_name, file_extension, content)
 
