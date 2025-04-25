ae5df5e9cfb0ee708d23f26bb86ff4568851de04
hrishikesh8848 <158465727+hrishikesh8848@users.noreply.github.com>
2025-04-25 08:54:43 +0530

    fix: erpnext 1980 permission error

--- frappe/model/document.py
+++ frappe/model/document.py
@@ -400,7 +400,7 @@ class Document(BaseDocument):
 		self.flags.ignore_version = frappe.flags.in_test if ignore_version is None else ignore_version
 
 		if self.get("__islocal") or not self.get("name"):
-			return self.insert()
+			return self.insert(ignore_permissions=True)
 
 		self.check_if_locked()
 		self._set_defaults()
