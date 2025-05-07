c928585bc762889581ac7bdeb3ff09fc628a5ede
bansodegaurav8848 <gaurav@8848digital.com>
2025-05-07 17:59:24 +0530

    fix: fix doc mapping issue

--- frappe/model/mapper.py
+++ frappe/model/mapper.py
@@ -15,7 +15,10 @@ def make_mapped_doc(method, source_name, selected_children=None, args=None):
 
 	Called from `open_mapped_doc` from create_new.js"""
 
-	method = frappe.get_attr(frappe.override_whitelisted_method(method))
+	for hook in reversed(frappe.get_hooks("override_whitelisted_methods", {}).get(method, [])):
+		# override using the first hook
+		method = hook
+		break
 
 	method = frappe.get_attr(method)
 
