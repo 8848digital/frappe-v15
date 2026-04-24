ac1e131f026e036350643fd817588061c30d2676
RitvikSardana <65544983+RitvikSardana@users.noreply.github.com>
2026-04-24 14:06:40 +0530

    fix: pytz to filter out deprecated timezones (#38751)
    (cherry picked from commit b484cf013680b325ab20be42a0e12c4530fe5360)
    
    # Conflicts:
    #	frappe/core/doctype/user/user.py
    #	frappe/patches.txt

--- frappe/core/doctype/user/user.py
+++ frappe/core/doctype/user/user.py
@@ -3,6 +3,11 @@
 
 from collections.abc import Iterable
 from datetime import timedelta
+<<<<<<< HEAD
+=======
+from functools import cached_property, lru_cache
+from typing import Any
+>>>>>>> b484cf0136 (fix: pytz to filter out deprecated timezones (#38751))
 
 import frappe
 import frappe.defaults
@@ -782,9 +787,20 @@ class User(Document):
 
 @frappe.whitelist()
 def get_timezones():
+<<<<<<< HEAD
 	import pytz
 
 	return {"timezones": pytz.all_timezones}
+=======
+	return {"timezones": _get_timezones()}
+
+
+@lru_cache(maxsize=1)
+def _get_timezones():
+	import pytz
+
+	return sorted(pytz.common_timezones)
+>>>>>>> b484cf0136 (fix: pytz to filter out deprecated timezones (#38751))
 
 
 @frappe.whitelist()
