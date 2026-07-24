1e344c4624b9ce23e64508655813f10216fa319b
Deepesh Garg <deepeshgarg6@gmail.com>
2026-07-24 12:59:12 +0530

    chore: resolve conflicts

--- frappe/permissions.py
+++ frappe/permissions.py
@@ -584,7 +584,9 @@ def remove_user_permission(doctype, name, user):
 	user_permission_name = frappe.db.get_value(
 		"User Permission", dict(user=user, allow=doctype, for_value=name)
 	)
-	frappe.delete_doc("User Permission", user_permission_name)
+	frappe.delete_doc(
+		"User Permission", user_permission_name, force=True, ignore_permissions=ignore_permissions
+	)
 
 
 def clear_user_permissions_for_doctype(doctype, user=None):
